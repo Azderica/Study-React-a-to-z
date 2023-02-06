dc6e415a06d1723964217e33035f03fa9c02796f
myepark <myepark@gmarket.com>
2023-02-07 08:11:00 +0900

    form.js 분리

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import Lists from "./components/Lists";
+import Form from "./components/Form";
 
 export default function App() {
   const [todoData, setTodoData] = useState([
@@ -16,10 +17,6 @@ export default function App() {
     },
   ]);
   const [value, setValue] = useState("");
-
-  const handleChange = (e) => {
-    setValue(e.target.value);
-  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -43,22 +40,7 @@ export default function App() {
 
         <Lists todoData={todoData} setTodoData={setTodoData} />
 
-        <form style={{ display: "flex" }} onSubmit={handleSubmit}>
-          <input
-            type="text"
-            name="value"
-            style={{ flex: "10", padding: "5px" }}
-            placeholder="해야 할 일을 입력하세요."
-            value={value}
-            onChange={handleChange}
-          />
-          <input
-            type="submit"
-            value="입력"
-            className="btn"
-            style={{ flex: "1" }}
-          />
-        </form>
+        <Form handleSubmit={handleSubmit} value={value} setValue={setValue} />
       </div>
     </div>
   );
