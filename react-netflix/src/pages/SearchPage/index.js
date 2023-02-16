159a8f22cb7cc0103c5ccd20927b21a6c5e8fc33
Azderica <mh97888@gmail.com>
2023-02-16 09:07:21 +0900

    search ui

--- react-netflix/src/pages/SearchPage/index.js
+++ react-netflix/src/pages/SearchPage/index.js
@@ -59,9 +59,7 @@ export default function SearchPage() {
     ) : (
       <section className="no-results">
         <div className="no-results__text">
-          <p>
-            {/* 찾고자하는 검색어"{debouncedSearchTerm}"에 맞는 영화가 없습니다. */}
-          </p>
+          <p>찾고자하는 검색어"{searchTerm}"에 맞는 영화가 없습니다.</p>
         </div>
       </section>
     );
